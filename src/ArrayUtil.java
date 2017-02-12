631f4aa530ac2347f73be45f8f768004151250c0
LEAUGE COMPUTER <DO NOT CHANGE THIS/>
2017-02-12 14:31:39 -0800

    Yay, houses.
    I decided to be an architect and built houses.

---
+++ src/ArrayUtil.java
@@ -0,0 +1,46 @@
+
+
+
+public class ArrayUtil {
+	
+	/** Reverse elements of array arr
+	*   Precondition: arr.length > 0.
+	*   Postcondition: The elements of arr have been reversed
+	*   @param arr the array to manipulate
+	*/
+	public static void reverseArray(int[] arr){
+		/* code goes here */
+	}
+}
+
+class Matrix{
+	private int[][] mat;
+	
+	public Matrix(int[][] m){
+		mat = m;
+	}
+	
+	/** Revereses the elements in each row of mat.
+	*   Postcondition: The elements in each row have been reversed
+	*/
+	public void reverseAllRows(){
+		/* code goes here */
+	}
+	
+	/** Reverses the elements of mat.
+	*   Postcondition:
+	*   - The final elements of mat, when read in row-major order, 
+	*     are the same as the original elements of mat when read
+	*     from the bottom corner, right to left, going upward.
+	*   - mat[0][0] contains what was originally the last element.
+	*   - mat[mat.length - 1][mat[0].length -1] contains what was
+	*     originally the first element.
+	*/
+	public void reverseMatrix(){
+		/* code goes here */
+	}
+	
+	public int[][] getIntArray(){
+		return mat;
+	}
+}
