9d39d9af616d9ddeda2963cf3f9165ad528ec0b4
MilesOei <league@iMac.attlocal.net>
2017-08-13 14:27:19 -0700

    Static Methods
    Much like Binary Converter, we have an unfinished Pig Latin Translator

---
+++ src/Level 1-Joanne/src/TastyEgg.java
@@ -0,0 +1,52 @@
+import java.util.Random;
+
+public class TastyEgg {
+	public static void main(String[] args) {
+		KinderSurprise myKinderSurprise = new KinderSurprise(new Egg(new Toy()));
+		// Your mission: Find out what toy is inside your KinderSurprise.
+		System.out.println(myKinderSurprise.getKinderSurpriseContents().getEggContents().getToyName());
+	}
+}
+
+class KinderSurprise {
+	private Egg egg;
+
+	public KinderSurprise(Egg egg) {
+		this.egg = egg;
+	}
+
+	Egg getKinderSurpriseContents() {
+		// ADD CODE HERE
+		return this.egg;
+	}
+}
+
+class Egg {
+
+	private Toy toy;
+
+	public Egg(Toy toy) {
+		this.toy = toy;
+	}
+
+	Toy getEggContents() {
+		// ADD CODE HERE
+		return this.toy;
+	}
+
+}
+
+class Toy {
+	String getToyName() {
+		int randomToySelector = new Random().nextInt(2);
+		if (randomToySelector == 0)
+			return "play doh";
+		else if (randomToySelector == 1)
+			return "train";
+		else
+			return "spongebob";
+	}
+
+}
+
+
